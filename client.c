3270e21323da92207ac0b7fed8e3731c7e18a3a4
Andrew Sealy <andrewjsealy@live.ca>
2017-07-08 16:50:26 -0600

    Add files via upload

--- client.c
+++ client.c
@@ -7,70 +7,75 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
+//this program uses a socket implementation from www.LinuxHowtos.org
 void error(const char *msg)
 {
-    perror(msg);
-    exit(0);
+	perror(msg);
+	exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+	int sockfd, portno, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-
-    while(1){
-    printf("Silent or Betray? (capital B or S): ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-	if(buffer[0]!='S'&&buffer[0]!='B'&&buffer[0]!='s'&&buffer[0]!='b'){
-		printf("Error in input...\n"); continue;
+	char buffer[256];
+	if (argc < 3) {
+		fprintf(stderr,"usage %s hostname port\n", argv[0]);
+		exit(0);
 	}
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+	portno = atoi(argv[2]);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) 
+		error("ERROR opening socket");
+	server = gethostbyname(argv[1]);
+	if (server == NULL) {
+		fprintf(stderr,"ERROR, no such host\n");
+		exit(0);
+	}
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, 
+		(char *)&serv_addr.sin_addr.s_addr,
+		server->h_length);
+	serv_addr.sin_port = htons(portno);
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+		error("ERROR connecting");
 
-printf("Play Again?[Y/N]: ");
-bzero(buffer, 256);
-fgets(buffer,255,stdin);
-if(buffer[0]=='y'||buffer[0]=='Y') continue;
-else{
-bzero(buffer,256);
-write(sockfd,"break",5);
-break;
+	//loop until user quits 
+	while(1){
+		//take input from user
+		printf("Silent or Betray? (capital B or S): ");
+		bzero(buffer,256);
+		fgets(buffer,255,stdin);
+		//check if input is valid 
+		if(buffer[0]!='S'&&buffer[0]!='B'&&buffer[0]!='s'&&buffer[0]!='b'){
+			printf("Error in input...\n"); continue;
+		}
+		//write to server
+		n = write(sockfd,buffer,strlen(buffer));
+		if (n < 0) 
+			error("ERROR writing to socket");
+		bzero(buffer,256);
+		n = read(sockfd,buffer,255);
+		if (n < 0) 
+			error("ERROR reading from socket");
+		printf("%s\n",buffer);
 
-}
-
-
-}
-    close(sockfd);
-    return 0;
+		//take input for play again
+		printf("Play Again?[Y/N]: ");
+		bzero(buffer, 256);
+		fgets(buffer,255,stdin);
+		//continue loop
+		if(buffer[0]=='y'||buffer[0]=='Y') continue;
+		//else write break to server and the break client
+		else{
+			bzero(buffer,256);
+			write(sockfd,"break",5);
+			break;
+		}
+	}
+	close(sockfd);
+	return 0;
 }
--- server.c
+++ server.c
@@ -1,5 +1,3 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+//this program uses a socket implementation from www.LinuxHowtos.org
 void error(const char *msg)
 {
     perror(msg);
@@ -16,59 +15,64 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
+ int sockfd, newsockfd, portno;
+ socklen_t clilen;
+ char buffer[256];
+ struct sockaddr_in serv_addr, cli_addr;
+ int n;
+ if (argc < 2) {
+     fprintf(stderr,"ERROR, no port provided\n");
+     exit(1);
+ }
+ sockfd = socket(AF_INET, SOCK_STREAM, 0);
+ if (sockfd < 0) 
+    error("ERROR opening socket");
+bzero((char *) &serv_addr, sizeof(serv_addr));
+portno = atoi(argv[1]);
+serv_addr.sin_family = AF_INET;
+serv_addr.sin_addr.s_addr = INADDR_ANY;
+serv_addr.sin_port = htons(portno);
+if (bind(sockfd, (struct sockaddr *) &serv_addr,
+  sizeof(serv_addr)) < 0) 
+  error("ERROR on binding");
+listen(sockfd,5);
+clilen = sizeof(cli_addr);
+newsockfd = accept(sockfd, 
+ (struct sockaddr *) &cli_addr, 
+ &clilen);
+if (newsockfd < 0) 
+  error("ERROR on accept");
+
+//loop until client kills server 
 while(1){
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
+ bzero(buffer,256);
+ n = read(newsockfd,buffer,255);
+ if (n < 0) error("ERROR reading from socket");
 
+   //if break is entered, kill server 
+ if(buffer[0]=='b'&&buffer[1]=='r'&&buffer[2]=='e'&&buffer[3]=='a'&&buffer[4]=='k') break;
+ printf("\n%c\n",buffer[0]);
 
-	if(buffer[0]=='b'&&buffer[1]=='r'&&buffer[2]=='e'&&buffer[3]=='a'&&buffer[4]=='k') break;
-     printf("\n%c\n",buffer[0]);
-	char clientChoice=buffer[0];
-	int randomChoice;
-	randomChoice=rand()%2;
-	printf("\n%d\n",randomChoice);
+   //client choice is from the buffer
+ char clientChoice=buffer[0];
+   //server choice is random
+ int randomChoice;
+ randomChoice=rand()%2;
 
-	char serverChoice;
-	if(randomChoice==0) serverChoice='S';
-	if(randomChoice==1) serverChoice='B';
+   //serChoice based on random
+ char serverChoice;
+ if(randomChoice==0) serverChoice='S';
+ if(randomChoice==1) serverChoice='B';
 
+   //prisoner dilemma based on client and server choices 
+ if(clientChoice=='S'&&serverChoice=='S') n = write(newsockfd,"Client: Silent, 1 year. Server: Silent, 1 year.",255);
+ if(clientChoice=='B'&&serverChoice=='S') n = write(newsockfd,"Client: Betray, 0 years. Server: Silent, 3 years.",255);
+ if(clientChoice=='S'&&serverChoice=='B') n = write(newsockfd,"Client: Silent, 3 years. Server: Betray, 0 years.",255);
+ if(clientChoice=='B'&&serverChoice=='B') n = write(newsockfd,"Client: Betray, 2 years. Server: Betray, 2 years.",255);
 
-	if(clientChoice=='S'&&serverChoice=='S') n = write(newsockfd,"Client: Silent, 1 year. Server: Silent, 1 year.",255);
-if(clientChoice=='B'&&serverChoice=='S') n = write(newsockfd,"Client: Betray, 0 years. Server: Silent, 3 years.",255);
-if(clientChoice=='S'&&serverChoice=='B') n = write(newsockfd,"Client: Silent, 3 years. Server: Betray, 0 years.",255);
-if(clientChoice=='B'&&serverChoice=='B') n = write(newsockfd,"Client: Betray, 2 years. Server: Betray, 2 years.",255);
-
-     if (n < 0) error("ERROR writing to socket");
+ if (n < 0) error("ERROR writing to socket");
 }
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
+close(newsockfd);
+close(sockfd);
+return 0; 
 }
